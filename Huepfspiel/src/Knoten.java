b1d44c17843dce9a6707d6a370c0312ca715f104
unknown <hellmann1.nico@gmx.de>
2024-02-22 06:49:34 +0100

    kinda initial commit

--- Huepfspiel/src/Knoten.java
+++ Huepfspiel/src/Knoten.java
@@ -1,4 +1,12 @@
-
 public class Knoten {
-
+	int id;
+	int[] verknuepfungen;
+	
+	
+	public Knoten(int id, int[] verknuepfungen)
+	{
+		this.id = id;
+		this.verknuepfungen = verknuepfungen;
+		
+	}
 }
